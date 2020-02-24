16ad474563ad4c9c40ee4bcd341ba850827a9ea7
Xavier Perez <xperez007@gmail.com>
2020-02-24 12:38:33 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -4,6 +4,12 @@ require_relative './dance_module.rb'
 class Dancer 
   include Dance 
   extend MetaDancing
+  extend
+FancyDance::ClassMethods 
+  include
+FancyDance::InstanceMethods
+end
+
 end
 
   attr_accessor :name 
